d27baa4c38cca0ae565f538114c5db16f1aa9d90
Luis Diego Lucero Navarro <LuisDiegoLuceroNavarro@gmail.com>
2021-12-07 20:30:32 -0700

    Se creo el php conexion

---
+++ conexion.php
@@ -0,0 +1,7 @@
+<?php  
+$servidor="localhost";
+$usuario="itesmeit_itesmei";
+$clave="^5zE]hcvfLRc";
+$base="itesmeit_empresa_DiegoLucero";
+$conexion = mysqli_connect($servidor,$usuario,$clave,$base);
+?>
